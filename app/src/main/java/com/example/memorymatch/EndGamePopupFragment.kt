d590918dcdb860447415ebcd832f2b86c9a0e7b0
Azro08 <azro.skib08@gmail.com>
2023-12-21 05:53:32 +0300

    finish

--- app/src/main/java/com/example/memorymatch/EndGamePopupFragment.kt
+++ app/src/main/java/com/example/memorymatch/EndGamePopupFragment.kt
@@ -1,17 +1,35 @@
 package com.example.memorymatch
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.memorymatch.databinding.FragmentEndGamePopupBinding
 
 class EndGamePopupFragment : Fragment() {
+    private var _binding: FragmentEndGamePopupBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_end_game_popup, container, false)
+    ): View {
+        _binding = FragmentEndGamePopupBinding.inflate(inflater, container, false)
+        return _binding!!.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val coins = arguments?.getInt("coins") ?: 0
+        binding.textViewCoins.text = coins.toString()
+        binding.imageViewHome.setOnClickListener {
+            findNavController().navigate(R.id.nav_end_start)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
